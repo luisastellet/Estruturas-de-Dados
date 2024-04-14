7b070eb7885b22206b8ed9f4caf36a4e27a8dece
luisastellet <luisastellet@id.uff.br>
2024-04-14 15:26:55 -0300

    lista 1

--- Lista_1/Q1.c
+++ Lista_1/Q1.c
@@ -6,20 +6,29 @@
 //(Q1) cópia de uma árvore: TAB* copia (TAB *a);
 
 
+// TAB* copia (TAB *a){
+//     TAB *aux = TAB_inicializa();
+//     if(!a) return a;
+//     //jeito mais fácil de entender
+//     TAB * x = copia(a->esq);
+//     TAB * y = copia(a->dir);
+//     aux = TAB_cria(a->info, x, y);
+//     return aux;
+
+//     //jeito mais complexo, mas que entendi depois
+//     // aux = TAB_cria(a->info, copia(a->esq), copia(a->dir));
+//     // return aux;
+// }
+
 TAB* copia (TAB *a){
-    TAB *aux = TAB_inicializa();
-    if(!a) return a;
-    //jeito mais fácil de entender
-    TAB * x = copia(a->esq);
-    TAB * y = copia(a->dir);
-    aux = TAB_cria(a->info, x, y);
-    return aux;
-
-    //jeito mais complexo, mas que entendi depois
-    // aux = TAB_cria(a->info, copia(a->esq), copia(a->dir));
-    // return aux;
+  TAB *aux = TAB_inicializa();
+  if(!a) return a;
+  //jeito mais fácil de entender
+  aux = TAB_cria(a->info, NULL, NULL);
+  aux->esq = copia(a->esq);
+  aux->dir = copia(a->dir);
+  return aux;
 }
-
 
 
 int main(void){
